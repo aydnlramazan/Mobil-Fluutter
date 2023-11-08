824a53da45e3d3055f30548ffca17aa28f7d53d1
aydnlramazan <106530954+aydnlramazan@users.noreply.github.com>
2023-11-08 22:46:38 +0300

    "soru cevap app"

---
+++ introapp_2/introapp_devam/lib/models/swiper_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:introapp_devam/home_screen.dart';
+import 'package:introapp_devam/models/result_screen.dart';
+import 'package:introapp_devam/quiz_screen.dart';
+
+class SwiperScreen extends StatefulWidget {
+  const SwiperScreen({Key? key}) : super(key: key);
+
+  @override
+  _SwiperScreenState createState() => _SwiperScreenState();
+}
+
+class _SwiperScreenState extends State<SwiperScreen> {
+  int total = 0;
+  int correct = 0;
+  int incorrect = 0;
+  List _answers = [];
+  List _correctAnswers = [];
+  List question = [];
+  String activeScreenName = "home-screen";
+  void goToPage(int a) {
+    if (a == 0) {
+      setState(() {
+        activeScreenName = "quiz-screen";
+      });
+    }
+    if (a == 1) {
+      setState(() {
+        activeScreenName = "final-screen";
+      });
+    }
+  }
+
+  void transferValues(int a, int b, int c, List d, List e, List f) {
+    total = a;
+    correct = b;
+    incorrect = c;
+    _answers = d;
+    _correctAnswers = e;
+    question = f;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activeScreen = HomeScreen(goToPage);
+    if (activeScreenName == "quiz-screen") {
+      activeScreen = QuizScreen(goToPage, transferValues);
+    }
+    if (activeScreenName == "final-screen") {
+      activeScreen = ResultScreen(
+          total, correct, incorrect, _answers, _correctAnswers, question);
+    }
+    return Container(
+      child: activeScreen,
+    );
+  }
+}
